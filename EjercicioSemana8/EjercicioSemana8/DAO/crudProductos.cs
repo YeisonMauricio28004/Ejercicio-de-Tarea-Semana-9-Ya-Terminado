229ab3ee0fdb7fb500d2172c81729cc079d1490f
YeisonMauricio28004 <116232628+YeisonMauricio28004@users.noreply.github.com>
2023-03-24 21:12:15 -0600

    Ejercicio de Tarea Semana 9 ya Terminado

---
+++ EjercicioSemana8/EjercicioSemana8/DAO/crudProductos.cs
@@ -0,0 +1,29 @@
+﻿using EjercicioSemana8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSemana8.DAO
+{
+    public class crudProductos
+    {
+        public void guardar(Producto paramProducto) 
+        {
+            using (AlmacenContext bd= new AlmacenContext()) 
+            { 
+              Producto producto = new Producto();
+                producto.Nombre = paramProducto.Nombre;
+                producto.Descripcion = paramProducto.Descripcion;
+                producto.Precio = paramProducto.Precio;
+                producto.Stock= paramProducto.Stock;
+
+                bd.Add(producto);
+                bd.SaveChanges();
+
+                Console.WriteLine("¡¡Su Producto Fue Ingresado Exitosamente!!");
+            }
+        }
+    }
+}
---
+++ EjercicioSemana8/EjercicioSemana8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using EjercicioSemana8.DAO;
+using EjercicioSemana8.Models;
+
+while (true)
+{
+    Console.WriteLine("Por Favor Seleccione una Opción del Menú");
+    Console.WriteLine("1 - Guardar");
+    Console.WriteLine("2 - Salir");
+    int op = int.Parse(Console.ReadLine());
+
+    switch (op)
+    {
+        case 1:
+            Producto producto = new Producto();
+            crudProductos crudProductos = new crudProductos();
+            Console.WriteLine("Ingrese el Nombre del Producto que desee llevar");
+            producto.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese la Descripción Producto que desee llevar");
+            producto.Descripcion = Console.ReadLine();
+            Console.WriteLine("Ingrese el Precio del Producto que desee llevar");
+            producto.Precio = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el Stock del Producto que desee llevar");
+            producto.Stock = int.Parse(Console.ReadLine());
+            break;
+        case 2:
+            break;
+    }
+    Console.WriteLine("¿Desea Volver a la Ejecución del Programa? SI= S / No= N");
+    String seguir = Console.ReadLine();
+    if(!(seguir.Equals ('S')))  
+    {
+        break;
+    }
+    
+}
+
+
+
+    
+
+
+
+
